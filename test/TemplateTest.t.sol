c23f440e7dfe9c10ab1b8c691b66e759a92c8327
cyotee doge <cyotee@gmail.com>
2025-02-01 16:05:35 -0800

    feat: init

---
+++ test/TemplateTest.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+// solhint-disable no-global-import
+// solhint-disable state-visibility
+// solhint-disable func-name-mixedcase
+import "forge-std/Test.sol";
+// import "contracts/test/foundry/extensions/TestExtended.sol";
+
+contract TemplateTest is Test {
+
+    function setUp()
+    public {
+    }
+
+    function test_poc(
+
+    ) public {
+    }
+
+}
